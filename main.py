774a59d5ec63b3fffa8af1f16e316136b8584f48
Niclas Lund <niclas.lund@telia.com>
2022-01-30 14:33:31 +0100

    refactoring for fork

---
+++ main.py
@@ -0,0 +1,10 @@
+from generator import *
+
+if __name__ == "__main__":
+    #random_person = Entity(True)
+    #fixed_person = Entity(False)
+    
+    x = create_player()
+    for y in x.values():
+        print(y)
+    
